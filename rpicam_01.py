5dcb3322a296807ecceccc18fec685fa1dbdd883
강민구 <sorrysam7879@gmail.com>
2026-03-18 15:59:20 +0900

    cam size & ExposureTime_03

--- rpicam_01.py
+++ rpicam_01.py
@@ -3,21 +3,19 @@ import time
 
 picam2 = Picamera2()
 
-# 네 카메라(5120x800)의 비율을 유지한 축소 미리보기
 config = picam2.create_preview_configuration(
     main={"size": (1280, 200), "format": "RGB888"}
 )
 picam2.configure(config)
 
-# 노출시간/게인 고정
 picam2.set_controls({
     "AeEnable": False,
     "ExposureTime": 50000,   # 50ms
     "AnalogueGain": 1.0
 })
 
-# 미리보기 창 시작 -> 그 다음 카메라 시작
-picam2.start_preview(Preview.QTGL, width=1280, height=200)
+# VNC/원격 화면에서는 QT가 더 적합
+picam2.start_preview(Preview.QT)
 picam2.start()
 
 try:
