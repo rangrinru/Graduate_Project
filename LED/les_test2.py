932232b63522903b5b147522da6d89e2f81d315c
강민구 <sorrysam7879@gmail.com>
2026-04-28 18:11:39 +0900

    GPIO2217

--- LED/les_test2.py
+++ LED/les_test2.py
@@ -1,10 +1,12 @@
 from gpiozero import LED
 from time import sleep
 
-relay = LED(22, active_high=False)
+relay_22 = LED(22, active_high=False)
+relay_17 = LED(17, active_high=False)
 
-
-relay.on()
+relay_22.on()
+relay_17.off()
 sleep(5)
-relay.off()
+relay_17.on()
+relay_22.off()
 sleep(5)
