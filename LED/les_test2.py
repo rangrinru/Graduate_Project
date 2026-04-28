653c214d788282199a2e397471a1e1a9e0d82a8f
강민구 <sorrysam7879@gmail.com>
2026-04-28 18:18:30 +0900

    sleep1

--- LED/les_test2.py
+++ LED/les_test2.py
@@ -3,10 +3,10 @@ from time import sleep
 
 relay_22 = LED(22, active_high=False)
 relay_17 = LED(17, active_high=False)
-
-relay_22.on()
-relay_17.off()
-sleep(5)
-relay_17.on()
-relay_22.off()
-sleep(5)
+while True:
+    relay_22.on()
+    relay_17.off()
+    sleep(1)
+    relay_17.on()
+    relay_22.off()
+    sleep(1)
